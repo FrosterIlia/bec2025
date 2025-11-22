d7fb07e8fb877d6d1ae395f43f1f617909046934
Ilia <iliamorozim@gmail.com>
2025-11-22 10:10:15 -0800

    3 sonic sensors added

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <IRremote.hpp>
 #include "PinDefinitionsAndMore.h"
 
+
+
+
 #define CH_MINUS 69
 #define CH 70
 #define CH_PLUS 71
@@ -36,13 +39,20 @@
 #define IN_3 8
 #define IN_4 7
 
-#define TRIGGER A4
-#define ECHO A5
+#define TRIGGER_1 A0
+#define ECHO_1 A1
+#define TRIGGER_2 A2
+#define ECHO_2 A3
+#define TRIGGER_3 A4
+#define ECHO_3 A5
 
 // Motor motor_left(IN_2, IN_1, EN_A, AUTO);
 // Motor motor_right(IN_4, IN_3, EN_B, AUTO);
 
-// Sonic sonic(ECHO, TRIGGER);
+Sonic sonic_1(ECHO_1, TRIGGER_1);
+Sonic sonic_2(ECHO_2, TRIGGER_2);
+Sonic sonic_3(ECHO_3, TRIGGER_3);
+
 
 int y = 255;
 int x;
@@ -69,7 +79,8 @@ void loop()
     IrReceiver.resume();
   }
 
-  // static unsigned long last_control_ms = 0;
+  static unsigned long last_control_ms = 0;
+  static unsigned long last_plot_ms = 0;
   // if (millis() - last_control_ms >= CONTROL_PERIOD_MS)
   // {
   //   distance = sonic.get_distance();
@@ -78,20 +89,22 @@ void loop()
   //   int left = y + x;
   //   int right = y - x;
 
-  //   motor_left.setSpeed(left);
-  //   motor_right.setSpeed(right);
+  //   // motor_left.setSpeed(left);
+  //   // motor_right.setSpeed(right);
 
   //   last_control_ms = millis();
   // }
 
   // static unsigned long last_plot_ms = 0;
-  // if (millis() - last_plot_ms >= PLOT_PERIOD_MS)
-  // {
-  //   Serial.print("{p(d:");
-  //   Serial.print(static_cast<int>(distance));
-  //   Serial.print(",s:");
-  //   Serial.print(2);
-  //   Serial.print(")}");
-  //   last_plot_ms = millis();
-  // }
+  if (millis() - last_plot_ms >= PLOT_PERIOD_MS)
+  {
+    Serial.print("{p(d1:");
+    Serial.print(sonic_1.get_distance());
+    Serial.print(",d2:");
+    Serial.print(sonic_2.get_distance());
+    Serial.print(",d3:");
+    Serial.print(sonic_3.get_distance());
+    Serial.print(")}");
+    last_plot_ms = millis();
+  }
 }

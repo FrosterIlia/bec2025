a8bceef10a5f99c8f7892a858795e4f9d8872751
Ilia <iliamorozim@gmail.com>
2025-11-22 10:00:00 -0800

    Remote controls defined

---
+++ include/irRemote.h
@@ -0,0 +1,24 @@
+#pragma once
+// codes for buttons
+#define POWER      0x45
+#define VOL_UP     0x46
+#define STOP       0x47
+#define REVERSE    0x44
+#define PLAY       0x40
+#define FORWARD    0x43
+#define DOWN       0x07
+#define VOL_DOWN   0x15
+#define UP         0x09
+#define ZERO       0x16
+#define EQ         0x25
+#define REPT       0x0D
+#define ONE        0x0C
+#define TWO        0x18
+#define THREE      0x5E
+#define FOUR       0x08
+#define FIVE       0x1C
+#define SIX        0x5A
+#define SEVEN      0x42
+#define EIGHT      0x52
+#define NINE       0x4A
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
 #include <Arduino.h>
 #include "motor.h"
 #include "sonic.h"
+#include <IRremote.hpp>
+#include "PinDefinitionsAndMore.h"
 
-#define RECEIVER_PIN A2
+#define CH_MINUS 69
+#define CH 70
+#define CH_PLUS 71
+#define PREV 68
+#define NEXT 64
+#define PLAY_PAUSE 67
+#define VOL_MINUS 7
+#define VOL_PLUS 21 
+#define EQ 9
+#define ZERO 22
+#define HUNDRED_PLUS 25
+#define TWO_HUNDRED_PLUS 13
+#define ONE 12
+#define TWO 24
+#define THREE 94
+#define FOUR 8
+#define FIVE 28 
+#define SIX 90
+#define SEVEN 66 
+#define EIGHT 82
+#define NINE 74
+
+#define IR_RECEIVE_PIN 2
 
 #define EN_A 5
 #define EN_B 6
@@ -15,10 +39,10 @@
 #define TRIGGER A4
 #define ECHO A5
 
-Motor motor_left(IN_2, IN_1, EN_A, AUTO);
-Motor motor_right(IN_4, IN_3, EN_B, AUTO);
+// Motor motor_left(IN_2, IN_1, EN_A, AUTO);
+// Motor motor_right(IN_4, IN_3, EN_B, AUTO);
 
-Sonic sonic(ECHO, TRIGGER);
+// Sonic sonic(ECHO, TRIGGER);
 
 int y = 255;
 int x;
@@ -31,35 +55,43 @@ const uint32_t PLOT_PERIOD_MS = 100;
 void setup()
 {
   Serial.begin(115200);
-  // pinMode(RECEIVER_PIN, INPUT);
+  IrReceiver.begin(IR_RECEIVE_PIN, 1);
 }
 
 void loop()
 {
 
-  static unsigned long last_control_ms = 0;
-  if (millis() - last_control_ms >= CONTROL_PERIOD_MS)
+  if (IrReceiver.decode())
   {
-    distance = sonic.get_distance();
-    const int x = map(distance, MIN_SONIC_DISTANCE, MAX_SONIC_DISTANCE, -255, 255);
+    Serial.println(IrReceiver.decodedIRData.command);
 
-    int left = y + x;
-    int right = y - x;
-
-    motor_left.setSpeed(left);
-    motor_right.setSpeed(right);
-
-    last_control_ms = millis();
+    delay(50);
+    IrReceiver.resume();
   }
 
-  static unsigned long last_plot_ms = 0;
-  if (millis() - last_plot_ms >= PLOT_PERIOD_MS)
-  {
-    Serial.print("{p(d:");
-    Serial.print(static_cast<int>(distance));
-    Serial.print(",s:");
-    Serial.print(2);
-    Serial.print(")}");
-    last_plot_ms = millis();
-  }
+  // static unsigned long last_control_ms = 0;
+  // if (millis() - last_control_ms >= CONTROL_PERIOD_MS)
+  // {
+  //   distance = sonic.get_distance();
+  //   const int x = map(distance, MIN_SONIC_DISTANCE, MAX_SONIC_DISTANCE, -255, 255);
+
+  //   int left = y + x;
+  //   int right = y - x;
+
+  //   motor_left.setSpeed(left);
+  //   motor_right.setSpeed(right);
+
+  //   last_control_ms = millis();
+  // }
+
+  // static unsigned long last_plot_ms = 0;
+  // if (millis() - last_plot_ms >= PLOT_PERIOD_MS)
+  // {
+  //   Serial.print("{p(d:");
+  //   Serial.print(static_cast<int>(distance));
+  //   Serial.print(",s:");
+  //   Serial.print(2);
+  //   Serial.print(")}");
+  //   last_plot_ms = millis();
+  // }
 }
